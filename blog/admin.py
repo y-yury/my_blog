a93e5e557104c172d3b01fe20da026e87369590f
Yury Sapazhkou <yury.sapozhkov@kapsch.by>
2016-11-22 21:30:51 +0300

    Admin part added to model (backend fields' extension with search)

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,9 @@ from .models import MyPost
 
 # Register your models here.
 
-admin.site.register(MyPost)
+
+class MyPostAdmin(admin.ModelAdmin):
+    list_display = ('author', 'date_created', 'date_published', 'title')
+    search_fields = ('text', 'title')
+
+admin.site.register(MyPost, MyPostAdmin)
